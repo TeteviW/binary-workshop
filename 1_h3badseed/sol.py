566955795a8f3ad8a7279da7194da2f66e526574
Tetevi <QT72132@umbc.edu>
2024-07-09 11:53:08 -0400

    Create a random number generator that matches a random sequence

--- 1_h3badseed/sol.py
+++ 1_h3badseed/sol.py
@@ -1,10 +1,17 @@
 from pwn import *
+from ctypes import CDLL
+
+libc = CDLL("libc.so.6")
+
+libc.srand(libc.time(0))
+
+final_time = libc.rand()
 
 io = process('./time')
 
 print(io.recvuntil(b'Enter your number: '))
 
-io.sendline(b'9090')
+io.sendline(b'%d' % final_time)
 
 print(io.recvline())
 
